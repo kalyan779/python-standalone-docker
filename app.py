bd57e0fcbd35c397cd4952c79db36bd8a3c3c9ee
kalyan779 <kalyan.chakravarthiy@gmail.com>
2019-12-30 21:58:51 +0530

    pushed docker code with python

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 from flask import Flask
 from redis import Redis
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    #count = redis.incr('hits')
+    return  '<h1 style-"color:red"> welcome to kalyanas docker app</h1>'
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", debug=True)
